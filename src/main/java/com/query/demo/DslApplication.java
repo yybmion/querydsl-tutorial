f6b99f12e3e118e5c82876e3bc34550f5e61df3c
yoobinmion <yyb400@ajou.ac.kr>
2023-12-06 14:25:37 +0900

    환경설정add

---
+++ src/main/java/com/query/demo/DslApplication.java
@@ -0,0 +1,13 @@
+package com.query.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslApplication.class, args);
+	}
+
+}
