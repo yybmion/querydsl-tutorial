52914cbfc861d8dec0305922b61d1ea1c6317ac0
yoobinmion <yyb400@ajou.ac.kr>
2023-12-23 17:06:13 +0900

    qeurydsl tutorial add

--- src/test/java/com/query/demo/QuerydslTest.java
+++ src/test/java/com/query/demo/QuerydslTest.java
@@ -68,9 +68,9 @@ public class QuerydslTest {
         Member result = queryFactory
                 .select(m)
                 .from(m)
-                .where(m.username.eq("yoobin"))
-                .fetchOne();
-        Assertions.assertThat(result.getUsername()).isEqualTo("yoobin");
+                .where(m.username.eq("yoobin"))   //where을 통해 member의 username yoobin과 같은 검색 조건
+                .fetchOne();        // 결과 하나를 가져와
+        Assertions.assertThat(result.getUsername()).isEqualTo("yoobin");  //그것이 yoobin인지 확인
     }
 
     @Test
@@ -80,9 +80,9 @@ public class QuerydslTest {
         Member result = queryFactory
                 .selectFrom(m)
                 .where(m.username.eq("yoobin")
-                        .and(m.age.eq(31)))
+                        .and(m.age.eq(31)))             // 조건 하나를 더 추가, 나이가 31과 같은 검색 조건
                 .fetchOne();
-        Assertions.assertThat(result.getUsername()).isEqualTo("yoobin");
+        Assertions.assertThat(result.getUsername()).isEqualTo("yoobin");  // 그것이 yoobin인지 확인
     }
 
     @Test
@@ -92,12 +92,12 @@ public class QuerydslTest {
 
         List<Member> result = queryFactory
                 .selectFrom(m)
-                .orderBy(m.age.desc().nullsLast())
-                .fetch();
+                .orderBy(m.age.desc().nullsLast())      // 나이를 내림차순으로 정렬
+                .fetch();           // list로 반환
 
-        Member member1 = result.get(0);
+        Member member1 = result.get(0);         // 그중 첫번째 가져와
 
-        Assertions.assertThat(member1.getAge()).isEqualTo(35);
+        Assertions.assertThat(member1.getAge()).isEqualTo(35);   //그 멤버의 나이가 35인지 확인
 
     }
 
@@ -109,11 +109,11 @@ public class QuerydslTest {
 
         List<Member> result = queryFactory
                 .selectFrom(m)
-                .orderBy(m.username.asc())
-                .offset(1)
-                .limit(3)
+                .orderBy(m.username.asc())          // username을 올림차순으로 정렬한다음
+                .offset(1)                          // 결과값중 첫번째부터
+                .limit(3)                           // 세번쨰 까지 가져온다.
                 .fetch();
-        Assertions.assertThat(result.size()).isEqualTo(3);
+        Assertions.assertThat(result.size()).isEqualTo(3);  // 따라서 개수는 3개
     }
 
     @Test
@@ -123,13 +123,13 @@ public class QuerydslTest {
 
         List<Member> result = queryFactory
                 .selectFrom(m)
-                .join(m.department, d)
-                .where(d.dName.eq("department1"))
+                .join(m.department, d)              // member와 department를 join시키고 별칭 d로 지정
+                .where(d.dName.eq("department1"))       // join시킨 테이블에서 dName이 department1과 이름이 같은 것을 검색 조건
                 .fetch();
 
         Assertions.assertThat(result)
-                .extracting("username")
-                .containsExactly("yoobin","jihyun","uiui");
+                .extracting("username") // 결과 list에서 username을 뽑아
+                .containsExactly("yoobin","jihyun","uiui"); // yoobin, jihyun,uiui 가 맞는지 확인
     }
 
     @Test
@@ -140,13 +140,13 @@ public class QuerydslTest {
         List<Tuple> result = queryFactory
                 .select(m.username , m.age.avg())
                 .from(m)
-                .groupBy(m.username)
-                .having(m.age.gt(25))
+                .groupBy(m.username)        // username으로 그룹화하고
+                .having(m.age.gt(25))   // 그중 25세 이상만 가져와
                 .fetch();
 
-        Tuple getResult = result.get(0);
+        Tuple getResult = result.get(0); // 첫전째 결과값만 가져와
 
-        Assertions.assertThat(getResult.get(m.username)).isEqualTo("minsu");
+        Assertions.assertThat(getResult.get(m.username)).isEqualTo("minsu");  // 그 이름이 minsu가 맞는지 확인
     }
 
     @Test
@@ -157,7 +157,7 @@ public class QuerydslTest {
         List<Member> results = queryFactory
                 .select(m)
                 .from(m)
-                .where(m.age.eq(
+                .where(m.age.eq(                        // 나이가 최대값인검색 조건에서 그 중 최대값과 나이가 같은 것만 검색함
                         JPAExpressions
                                 .select(m.age.max())
                                 .from(m)
@@ -178,7 +178,7 @@ public class QuerydslTest {
         List<Member> results = queryFactory
                 .select(m)
                 .from(m)
-                .where(m.age.in(
+                .where(m.age.in(                            // 29보다 낮은 나이 검색 조건인 나이와 딱 같으면 검색
                         JPAExpressions
                                 .select(m.age)
                                 .from(m)
@@ -198,7 +198,7 @@ public class QuerydslTest {
         QMember m = new QMember("m");
 
         List<Tuple> result = queryFactory
-                .select(m.username, Expressions.constant("CALL"))
+                .select(m.username, Expressions.constant("CALL"))       // CALL 상수를 username 뒤에 더해준다.
                 .from(m)
                 .fetch();
 
@@ -216,7 +216,7 @@ public class QuerydslTest {
                 .select(m.username.concat("_").concat(m.age.stringValue()).prepend("HELLO_"))
                 .from(m)
                 .fetch();
-
+                                                    // username뒤에 _ 더하고 나이를 붙여준다. 앞에는 HELLO_를 더한다.
         String results = result.get(0);
         String results2 = result.get(1);
 
